91ed1059e02c2150688d56aaea3dc2512ab2d5bf
Pham Hoang Anh <117793920+vjintageboy@users.noreply.github.com>
2024-05-26 23:58:32 +0700

    thêm slide và phần dưới footer, fix các lỗi trước

---
+++ script.js
@@ -0,0 +1,45 @@
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 1,
+    centeredSlides: true,
+    centeredSlidesBounds: true,
+    spaceBetween: 1,
+    slideToClickedSlide: true,
+    loopAdditionalSlides: 1,
+    loopFillGroupWithBlank: true,
+    autoHeight: true,
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: false,
+    breakpoints: {
+        320: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        480: {
+            slidesPerView: 2,
+            spaceBetween: 30
+        },
+        640: {
+            slidesPerView: 1.268,
+            spaceBetween: 20
+        }
+    }
+});
+const stopButton = document.querySelector('.stop-button');
+stopButton.addEventListener('click', function() {
+    swiper.autoplay.stop(); // Dừng autoplay
+});
+
+// Lấy ra nút bắt đầu và gán sự kiện click
+const startButton = document.querySelector('.start-button');
+startButton.addEventListener('click', function() {
+    swiper.autoplay.start(); // Bắt đầu autoplay
+});
